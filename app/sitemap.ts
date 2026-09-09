2b913f74b2a319fe58f933ad0f0cbfb9a6c43c47
urbanfreightinc21 <urbanfreightinc21@gmail.com>
2026-09-08 21:41:04 -0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,14 +10,39 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: `${baseUrl}/wear`,
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/events`,
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/about`,
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/archive`,
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/contact`,
       changeFrequency: "monthly",
       priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      changeFrequency: "yearly",
+      priority: 0.2,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      changeFrequency: "yearly",
+      priority: 0.2,
     },
   ];
 }
